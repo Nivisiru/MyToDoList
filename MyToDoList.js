5543ff9c3fd14c4b76cf47c51a62fc15a34c40f7
Nivisiru <143357249+Nivisiru@users.noreply.github.com>
2023-09-12 19:51:14 -0700

    update local

--- MyToDoList.js
+++ MyToDoList.js
@@ -1,28 +1,59 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const todoList = document.getElementById("todoList");
-    const addTodoButton = document.getElementById("addTodoButton");
-  
-    addTodoButton.addEventListener("click", function() {
-      const taskInput = document.getElementById("task");
-      const taskText = taskInput.value;
-  
-      // if (taskText !== "") {
-        const newTodo = document.createElement("li");
-        newTodo.innerText = taskText;
-  
-        const removeButton = document.createElement("button");
-        removeButton.innerText = "x";
-  
-        newTodo.appendChild(removeButton);
-        todoList.appendChild(newTodo);
-  
-        taskInput.value = "";
-      }
+document.addEventListener("DOMContentLoaded", function () {
+  const todoList = document.getElementById("todoList");
+  const addTodoButton = document.getElementById("addTodoButton");
+  const taskInput = document.getElementById("task");
+
+  const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+
+  function updateTodoList() {
+    todoList.innerHTML = "";
+
+    savedTodos.forEach((taskText, index) => {
+      const newTodo = document.createElement("li");
+      newTodo.innerText = taskText;
+
+      const removeButton = document.createElement("button");
+      removeButton.innerText = "x";
+
+      removeButton.addEventListener("click", function () {
+        savedTodos.splice(index, 1);
+        updateTodoList();
+        saveTodosToLocalStorage();
+      });
+
+      newTodo.appendChild(removeButton);
+      todoList.appendChild(newTodo);
     });
-  
-    todoList.addEventListener("click", function(e) {
-      const target = e.target;
-        target.parentElement.remove();
-      
-    });
+  }
+
+  function saveTodosToLocalStorage() {
+    localStorage.setItem("todos", JSON.stringify(savedTodos));
+  }
+
+  updateTodoList();
+
+  addTodoButton.addEventListener("click", function () {
+    const taskText = taskInput.value;
+
+    if (taskText !== "") {
+      savedTodos.push(taskText);
+      saveTodosToLocalStorage();
+
+      taskInput.value = "";
+
+      updateTodoList();
+    }
   });
+
+  todoList.addEventListener("click", function (e) {
+    const target = e.target;
+    target.parentElement.remove();
+
+    const taskText = target.parentElement.innerText;
+    const index = savedTodos.indexOf(taskText);
+    if (index !== -1) {
+      savedTodos.splice(index, 1);
+      saveTodosToLocalStorage();
+    }
+  });
+});
